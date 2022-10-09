04fe7cdc3eb9ebd747a358903c51b6d364373792
RajavJain <rajavjain2002@gmail.com>
2022-10-09 21:34:51 +0530

    Final Commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = 80;
+const port = 8000;
 const bodyparser=require("body-parser");
 
 const mongoose = require('mongoose');
